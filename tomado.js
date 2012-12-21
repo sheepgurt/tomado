a9981ccf920d0944a06361bb4989ed6b905fbd70
sheepgurt <geert@shuffler.fm>
2012-12-21 10:22:53 +0100

    [#41522805] It's alive!

--- tomado.js
+++ tomado.js
@@ -6,6 +6,7 @@ $(function() {
 		e.preventDefault(); // prevents browser re-postion after click
 		var a = $(e.target);
 		embedVideo(a.data("ytid"));
+		getBio(a.data("name"));
 	})
 
 });
@@ -17,11 +18,12 @@ function getVideos() {
 			var li = $('<li>');
 			var a 		= $('<a href="#">');
 
-			var parts = data[i]['object']['stream']['url'].split('/'); //makes array
+			var parts = data[i]['object']['stream']['url'].split('/'); //makes array out of the URL, indexing by every / ex: bla/bla/bla ['bla', 'bla', 'bla']
 			var ytid = parts[parts.length - 1]; //-1 because length counts from 1, array's are indexed starting by 0
 			a.data('ytid', ytid); // creates <a data-ytid = variable ytid | data attributes by John Resig
-			var artist 	= data[i]['object']['metadata']['artist']['name'];
-			var title 	= data[i]['object']['metadata']['title'];
+			var artist 	= data[i]['object']['metadata']['artist']['name']; //filter artist out of the json object
+			var title 	= data[i]['object']['metadata']['title']; //filter the title out of the json object
+			a.data('name', artist)
 
 			a.html( artist + " – " + title);
 			li.append(a);
@@ -30,8 +32,18 @@ function getVideos() {
 	} );
 }
 
-function embedVideo(p) {
-	var s = '<iframe width="560" height="315" src="http://www.youtube.com/embed/' + p + '?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'
+function embedVideo(id) {
+	var s = '<iframe width="560" height="315" src="http://www.youtube.com/embed/' + id + '?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'
 
 	$('iframe').replaceWith(s);
+}
+
+function getBio(name) {
+	var url = "http://developer.echonest.com/api/v4/artist/biographies?api_key=WFUMDKXDOYQOGU6GH&format=json&results=1&start=0&license=cc-by-sa&name=" + name
+
+	$.getJSON( url, function(data) { //request url, sends the json file to the parameter called data.
+		$('.bio').html(data['response']['biographies'][0]['text']); //.html replaces the existing html in <p class=bio><div> with the 'text' that is in the json file
+	});
+
+
 }
